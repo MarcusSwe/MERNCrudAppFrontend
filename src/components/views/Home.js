26892a5d92076be959f391453176a59966d7419e
unknown <marcus.larsen.swe@gmail.com>
2021-09-06 22:49:42 +0200

    so it begins

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext  } from "../../context/AuthContext";
+
+
+
+
 
 export const Home = () => {
+
+    const {isAuth} = useContext(AuthContext);
+
+    const isAuthyes = () => {
+        return (
+            <>
+            INLOGGAD
+            </>
+        )
+    }
+
+    const unAuth = () => {
+        return (
+        <>
+        hästen
+        </>
+        )
+    }
+
     return (
         <>
             <h1>Home</h1>
             <h3>Routing Turorial</h3>
+
+            {isAuth ? isAuthyes() : unAuth()}
         </>
     )
 }

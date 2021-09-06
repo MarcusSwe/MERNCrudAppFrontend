1737fa5c39a80f8f22cc5b453a698159dd372c00
unknown <marcus.larsen.swe@gmail.com>
2021-09-06 19:18:16 +0200

    navbar first test

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { NavLink, useHistory } from "react-router-dom";
-
-
+import omegapog2 from '../../omegapog2.png';
+import notes2 from '../../notes2.png';
+import '../../index.css';
 
 export const Navbar = () => {
 
@@ -18,21 +19,49 @@ export const Navbar = () => {
 
     const authNavbar = () => {
         return (
-            <>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/loginregister">LoginRegister</NavLink>
+            
+            <div className="topmenuWrapper">
 
-            </>
+             <div className="firstMenu">
+             <button className="newNote2" src={omegapog2} alt ="nein"></button>
+              <img src={notes2} alt ="nein" className="logga"></img>
+        
+              <button className="topMenuButtons">New Note</button>
+              <button className="topMenuButtons">Logout</button>
+              
+             
+        
+              <p className="topMenuLoginState">Not Logged In</p>
+
+        
+            </div>
+            
+
+          </div>
+          
         )        
     }
 
 
     const unAuthNavbar = () => {
         return (
-            <>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/loginregister">LoginRegister</NavLink>
-            </>
+            <div className="topmenuWrapper">
+
+             <div className="firstMenu">
+             <button className="newNote2" src={omegapog2} alt ="nein"></button>
+              <img src={notes2} alt ="nein" className="logga"></img>
+        
+              <NavLink to="/"><button className="topMenuButtons">Home</button></NavLink>
+              <NavLink to="/loginregister"><button className="topMenuButtons">LoginRegister</button></NavLink>        
+              
+        
+              <p className="topMenuLoginState">Not Logged In</p>
+
+        
+            </div>
+            
+
+          </div>
         )
     }
 

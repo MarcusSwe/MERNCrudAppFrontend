06d18ef70c369f51b73c8e96c6b25028cf6601bf
unknown <marcus.larsen.swe@gmail.com>
2021-09-07 22:18:37 +0200

    new post soon

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,37 +1,52 @@
 import React, { useContext, useState } from "react";
 import { AuthContext  } from "../../context/AuthContext";
 import Printa from '../general/Printa';
-
+import tagFun from '../../fun10.png';
+import tagMeta from '../../meta10.png';
+import tagUrgent from '../../urgent10.png';
 
 
 
 export const Home = () => {
 
     const [testNote, setTestNote] = useState([
-        {noteID: 1, theme: 0, author: "testUser", title: "title", message: "asfkjasdflökj asdflkjsdaf asdlfkjsad asdklfj asdfölkjs dsflöksjdf  sadfklösdjf asdfaf",
+        {theme: 0, author: "testUser", title: "title", message: "asfkjasdflökj asdflkjsdaf asdlfkjsad asdklfj asdfölkjs dsflöksjdf  sadfklösdjf asdfaf",
     date: "2021-09-09", tags:{fun: true, meta:true, urgent:true}, upvote: 5, replays: 2, 
     replayss:[{replayUser: "replayUser", replayText: "replaytext"},
      {replayUser: "replayUser2", replayText: "replaytext2"}]
     },
-    {noteID: 2, theme: 1, author: "testUser2", title: "title2", message: "23456456 34564356 asdlfkjsad asd4356j asdfölkjs dsflöksjdf  sadfklösdjf asdfaf",
+    {theme: 1, author: "testUser2", title: "title2", message: "23456456 34564356 asdlfkjsad asd4356j asdfölkjs dsflöksjdf  sadfklösdjf asdfaf",
     date: "2022-09-09", tags:{fun: true, meta:true, urgent:false}, upvote: 1, replays: 3, 
     replayss:[{replayUser: "replayUser10", replayText: "replaytext2"},
      {replayUser: "replayUser22", replayText: "replaytext22"},
      {replayUser: "replayUser2432", replayText: "replaytext2432"}]
     },
-    {noteID: 3, theme: 1, author: "testUser3", title: "title3", message: "oemga ösdjf asdfaf",
+    {theme: 1, author: "testUser3", title: "title3", message: "oemga ösdjf asdfaf",
     date: "2022-09-09", tags:{fun: false, meta:false, urgent:true}, upvote: 1, replays: 0, 
     replayss:[]
     },    
-    {noteID: 4, theme: 1, author: "testUser3", title: "title3", message: "oemga ösdjf asdfaf",
+    {theme: 1, author: "testUser3", title: "title3", message: "oemga ösdjf asdfaf",
     date: "2022-09-09", tags:{fun: false, meta:false, urgent:false}, upvote: 1, replays: 1, 
     replayss:[{replayUser: "replayUser10", replayText: "replaytext2"}]
     }
     ])
 
-    const addNote = (addAuthor, addTitle, addMessage, addDate, addTags) => {
+    const [showNewPost, setShowNewPost] = useState(false);
+
+    const openNewpost = () => {        
+        if(showNewPost){
+            return `individualNote2`
+        } else return `individualNote2 hidden`
+    }
+
+    const callOpenUpNewPost = () => {        
+        setShowNewPost(!showNewPost)
+    }
+
+
+    const addNote = (addTheme, addAuthor, addTitle, addMessage, addDate, tagFun, tagMeta, tagUrgent) => {
         const changed = [...testNote];
-        changed.push({noteID: 0, theme: 0, author: addAuthor, title: addTitle, message: addMessage, date: addDate, tags: addTags, upvote:0, replays:0, replayss:[]})
+        changed.push({theme: addTheme, author: addAuthor, title: addTitle, message: addMessage, date: addDate, tags: {fun:tagFun, meta: tagMeta, urgent: tagUrgent}, upvote:0, replays:0, replayss:[]})
         setTestNote(changed);
     }
 
@@ -55,17 +70,57 @@ export const Home = () => {
 
     const isAuthyes = () => {
         return (
-            <>
-            INLOGGAD
-            </>
+            <div style={{marginBottom:"45px",}}>
+            <button className="topMenuButtonsX" onClick={e => {callOpenUpNewPost()}}>Add new note</button>
+
+            <div className={openNewpost()}>
+                <div className="newNoteTopTitle"><b>New Note</b></div>
+      
+                <div>
+                    <form>
+                    <label for="constr-title" className="titleFont"><b>New Note Title</b></label>
+                    <input id="constr-title" type="text" name="constr-title" value="" className="inputTitle" maxlength="12" size="24" />
+                    <br/>
+
+                    <div className="checkboxWrapper">
+                        <label className="xCheckbox">
+                        <input type="checkbox" name="tag1" value="F"/>
+                        <span className="checkmark"></span>
+                        <img src={tagFun} className="funlogga"/>
+                        </label>
+
+                        <label className="xCheckbox">
+                        <input type="checkbox" id="constr-tag2" name="tag2" value="M" />
+                        <span className="checkmark"></span>
+                        <img src={tagMeta} className="metalogga"/>
+                        </label>
+
+                        <label className="xCheckbox">
+                        <input type="checkbox" id="constr-tag3" name="tag3" value="U" />
+                        <span className="checkmark"></span>
+                        <img src={tagUrgent} className="urgentlogga"/>
+                        </label>
+                    </div>
+
+                <label for="constr-subject" className="newNoteSubjectTitle"><b>New Note Subject</b></label>
+                <textarea id="constr-subject" rows="12" cols="24" className="subjectTextArea" maxlength="275"></textarea><br/>
+
+                <input type="button" id="constr-submit" name="submit" value="Submit" className="submitNoteButton"/>
+                <input type="button" id="constr-close" name="close" value="close" onClick={e => {callOpenUpNewPost()}}/>
+                </form>
+            </div>
+
+            </div>
+
+            </div>
         )
     }
 
     const unAuth = () => {
         return (
-        <>
-        hästen
-        </>
+        <div style={{marginBottom:"45px",}}>
+            <button className="topMenuButtonsX">Please login to post</button>
+        </div>
         )
     }
 

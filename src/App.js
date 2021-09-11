f30d6216bd070c1262c1434ace99a790f1700c2a
unknown <marcus.larsen.swe@gmail.com>
2021-09-11 23:32:14 +0200

    404 page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 
 import React from "react";
 import {  BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { Home } from './components/views/Home';
 import { Login } from './components/views/Login';
 import { Register } from './components/views/Register';
 import { Navbar } from './components/general/Navbar';
 import { UnPrivateRoute } from "./hocs/UnPrivateRoute";
 
+
+const Page404 = ({ location }) => (
+  <div>
+    <h2>No match found for <code>{location.pathname}</code></h2>
+  </div>
+);
+
 function App() {
   return (
     
     <Router>
-      <Navbar />
-      <Route exact path="/" component={Home} />
-      <UnPrivateRoute path ="/login" component={Login} />     
-      <UnPrivateRoute path ="/register" component={Register} />      
+      
+        <Navbar />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <UnPrivateRoute path ="/login" component={Login} />     
+        <UnPrivateRoute path ="/register" component={Register} />      
+        <Route component={Page404} />
+      </Switch>
     </Router>
 
   );
